248bad1baf717c836a6d9f47abe0cd8986de476e
Hamza <lelouch.hamza@gmail.com>
2019-07-16 18:37:40 +0100

    feat: Add unauthenticated login route

---
+++ src/Routes/index.js
@@ -0,0 +1,31 @@
+import React, { Suspense } from "react";
+import { Switch, Route } from "react-router-dom";
+import UnauthenticatedRoute from "Components/Auth/UnauthenticatedRoute";
+import AuthenticatedRoute from "Components/Auth/AuthenticatedRoute";
+
+// lazy loaded routes
+const Login = React.lazy(() => import("Containers/Login/Login"));
+
+export default ({ isAuthenticated }) => {
+  return (
+    <Switch>
+      {isAuthenticated && <Route render={() => <div>....loading</div>} />}
+      <UnauthenticatedRoute
+        isAuthenticated={isAuthenticated}
+        path="/login"
+        exact
+        component={() => (
+          <Suspense fallback={<div>Loading....</div>}>
+            <Login />
+          </Suspense>
+        )}
+      />
+      <AuthenticatedRoute
+        isAuthenticated={isAuthenticated}
+        path="/"
+        exact
+        component={props => <div />}
+      />
+    </Switch>
+  );
+};
