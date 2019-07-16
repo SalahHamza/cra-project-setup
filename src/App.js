96641029ba6a9bccc480656ffb9a7b6bf158b6c2
Hamza <lelouch.hamza@gmail.com>
2019-07-16 18:38:18 +0100

    feat: Add routes to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import storeContext from "./context";
+import Routes from "./Routes";
 
 class App extends Component {
   render() {
-    const store = {};
+    const store = {
+      isAuthenticated: false
+    };
     return (
       <storeContext.Provider value={store}>
-        <div>React App</div>
+        <Routes isAuthenticated={store.isAuthenticated} />
       </storeContext.Provider>
     );
   }
